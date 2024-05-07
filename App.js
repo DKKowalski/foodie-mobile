68dd9bf560c7dded58f6c747bf2f77cc437d3bff
DKKowalski <sewunakpandana5@gmail.com>
2024-05-07 11:26:04 +0100

    Foodie-mobile setup

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.text}>I am working </Text>
+        <Text style={styles.text}>Hello World!</Text>
+        <Text style={styles.text}>Don't go!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#153d6c",
+    padding: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+
+  text: {
+    color: "white",
   },
 });
